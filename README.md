5b2e4d91a72bc9f98e1245f3a52e95eae5687118
Ruben Pin <pinchasov.ruben@gmail.com>
2023-04-06 15:27:02 +0200

    updated tester for public use

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# push_swap_tester
+#push_swap_tester
 Simple input, output and memory leak tester for 42 project push_swap
+#Bash Script for Testing Push_Swap
+This repository contains a bash script for testing a push_swap program, in combination with a checker file.
+Include it in your Makefile for combining with multiple tests.
+
+#Prerequisites
+Bash shell
+Push_Swap program
+Checker program
+
+#Usage
+Run the tester.sh script with the following command:
+
+```
+./test_push_swap.sh <min> <max> <count> [<flags>] [<type>]
+```
+* <min>: the minimum value for the random number generator.
+* <max>: the maximum value for the random number generator.
+* <count>: the number of random numbers to generate.
+* <flags> (optional): the test flags to use:
+* 1: add a non-valid character.
+* 2: add a number outside of the range.
+* 3: add a duplicate number.
+* <type> (optional): the input type to use:
+* 1: input as a single string.
+* 2: input as two strings.
+
+For example, to test push_swap with a range of -100 to 100, generating 500 random numbers, and adding a non-valid character flag, you would use the following command:
+
+```
+./test_push_swap.sh -100 100 500 1
+```
+Output
+The script generates random numbers within the given range and tests the push_swap program with them. The output displays whether the push_swap program output is correct or not.
+
+If the output desired and there are no memory-leaks the checker program returns "OK" otherwise it will show "KO"
+
+```
+Output: OK
+Valgrind: OK
+```
+
+#Note
+This bash script only works in combination with a checker file. Please ensure that the checker program is in the same directory as the push_swap program before running the script.
+
--- tester.sh
+++ tester.sh
@@ -4,7 +4,7 @@
 if [ $# -lt 3 ]; then
     echo "Usage: $0 <min> <max> <count> [<flags>] [<type>]"
     echo "Flags: 1 = add non-valid char, 2 = add number outside range, 3 = add duplicate"
-	echo "Type: string = args as one string, mixed = args as string & individual"
+	echo "Type: 1 = args as one string, 2 = args as string & individual"
     exit 1
 fi
 
@@ -15,6 +15,11 @@ flag=${4:-0}
 typ=${5:-0}
 INT_MIN=-2147483648
 INT_MAX=2147483647
+
+GREEN="\033[32m"
+YELLOW="\033[33m"
+RED="\033[31m"
+RESET="\033[0m"
 
 # Check range
 if [ "$min" -gt "$max" ]; then
@@ -37,7 +42,7 @@ done
 if [ "$flag" -eq 1 ]; then
     index=$((RANDOM % count))
     numbers[$index]='u'
-	output_desired="ERROR"
+	output_desired="Error"
 fi
 
 # Add number outside range
@@ -50,7 +55,7 @@ if [ "$flag" -eq 2 ]; then
         num=$((INT_MIN - 1 - RANDOM))
     fi
     numbers[$index]=$num
-	output_desired="ERROR"
+	output_desired="Error"
 fi
 
 # Add duplicate number
@@ -58,56 +63,39 @@ if [ "$flag" -eq 3 ]; then
     index1=$((RANDOM % count))
     index2=$((RANDOM % count))
     numbers[$index1]=${numbers[$index2]}
-	output_desired="ERROR"
+	output_desired="Error"
 fi
 
-success="OK"
-fail="KO"
-error="Error"
-# Print numbers and call cprogram
-echo -e "\033[33mGenerated random numbers:\033[0m"
+# Adjust input (multiple arguments, string or mixed)
 if [ "$typ" -eq 2 ]; then 
 	group1=($(echo "${numbers[@]}" | cut -d' ' -f -$((count/2))))
 	group2=($(echo "${numbers[@]}" | cut -d' ' -f $(((count/2)+1))-))
 	output_desired="Error"
-	echo -e "\033[33m"./push_swap "${group1[@]}" '"'"${group2[*]}"'"'"\033[0m"
-	echo $(valgrind --log-file=testfile ./push_swap "${group1[@]}" "${group2[*]}")
+	#Uncomment the following line to output used arguments:
+	#echo -e ${YELLOW}"./push_swap ${group1[@]} \"${group2[*]}\""${RESET}
+	echo $(valgrind --log-file=testfile ./push_swap "${group1[@]}" "${group2[*]}" > /dev/null)
 	output_new=$(./push_swap "${group1[@]}" "${group2[*]}" | ./checker "${group1[@]}" "${group2[*]}" 2>&1)
-	#Print Checks
-	if [ "$output_new" = "$output_desired" ]; then
-  		echo -e "Output:\t\t \033[32mOK\033[0m"
-	else
-  		echo -e "Output:\t\t \033[31mKO\033[0m"
-	fi
-	echo -e "Valgrind:\t" $(awk '/in use at exit: 0 bytes/{f=1; printf "\033[0;32mOK\033[0m\n"; exit} /in use at exit:/{f=1; printf "\033[0;31mKO\033[0m\n"; exit} END{if(!f) printf "\033[0;31mKO\033[0m\n"}' testfile)
-	cat testfile | grep --color -A2 "in use at exit"
 elif [ "$typ" -eq 1 ]; then 
-	echo -e "\033[33m"./push_swap '"'"${numbers[*]}"'"'"\033[0m"
-	echo $(valgrind --log-file=testfile ./push_swap "${numbers[*]}")
+	#Uncomment the following line to output used arguments:
+	#echo -e ${YELLOW}"./push_swap \"${numbers[*]}\""${RESET}
+	echo $(valgrind --log-file=testfile ./push_swap "${numbers[*]}" > /dev/null)
 	output_new=$(./push_swap "${numbers[*]}" | ./checker "${numbers[*]}" 2>&1)
-	#Print Checks
-	if [ "$output_new" = "$success" ]; then
-  		echo -e "Output:\t\t \033[32mOK\033[0m"
-	elif [ "$output_new" = "$fail" ]; then
-  		echo -e "Output:\t\t \033[31mKO\033[0m"
-	elif [ "$output_new" = "$output_desired" ]; then
-  		echo -e "Output:\t\t \033[31mOK\033[0m"
-fi
-	echo -e "Valgrind:\t" $(awk '/in use at exit: 0 bytes/{f=1; printf "\033[0;32mOK\033[0m\n"; exit} /in use at exit:/{f=1; printf "\033[0;31mKO\033[0m\n"; exit} END{if(!f) printf "\033[0;31mKO\033[0m\n"}' testfile)
-	cat testfile | grep --color -A2 "in use at exit"
 else 
-	echo -e "\033[33m"./push_swap "${numbers[@]}""\033[0m"
-	echo $(valgrind --log-file=testfile ./push_swap "${numbers[@]}")
+	#Uncomment the following line to output used arguments:
+	#echo -e ${YELLOW}"./push_swap ${numbers[@]}"${RESET}
+	echo $(valgrind --log-file=testfile ./push_swap "${numbers[@]}" > /dev/null)
 	output_new=$(./push_swap "${numbers[@]}" | ./checker "${numbers[@]}" 2>&1)
-	#Print Checks
-	if [ "$output_new" = "$success" ]; then
-  		echo -e "Output:\t\t \033[32mOK\033[0m"
-	elif [ "$output_new" = "$fail" ]; then
-  		echo -e "Output:\t\t \033[31mKO\033[0m"
-	elif [ "$output_new" = "$output_desired" ]; then
-  		echo -e "Output:\t\t \033[31mOK\033[0m"
-	fi
-
-	echo -e "Valgrind:\t" $(awk '/in use at exit: 0 bytes/{f=1; printf "\033[0;32mOK\033[0m\n"; exit} /in use at exit:/{f=1; printf "\033[0;31mKO\033[0m\n"; exit} END{if(!f) printf "\033[0;31mKO\033[0m\n"}' testfile)
-	cat testfile | grep --color -A2 "in use at exit"
 fi
+
+#Uncomment the following line to show checker result:
+#echo ${output_new}
+
+# Print Checks
+if [ "$output_new" = "$output_desired" -o "$output_new" = "OK" ]; then
+	echo -e "Output:\t\t"${GREEN}"OK"${RESET}
+else
+	echo -e "Output:\t\t"${RED}"KO"${RESET}
+fi
+echo -e "Valgrind:\t"$(awk '/in use at exit: 0 bytes/{f=1; printf "'"${GREEN}"'OK'"${RESET}"'\n"; exit} /in use at exit:/{f=1; printf "'"${RED}"'KO'"${RESET}"'\n"; exit} END{if(!f) printf "'"${RED}"'KO'"${RESET}"'\n"}' testfile)
+#Uncomment the following line to see valgrind results:
+#cat testfile | grep --color -A2 "in use at exit"
